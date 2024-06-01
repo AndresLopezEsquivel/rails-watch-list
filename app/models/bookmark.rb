d6f4c2b5c3316926a15072a32e512d2d5429d1f7
Andres Lopez Esquivel <lopezesquivelandres1@gmail.com>
2024-06-01 14:09:47 -0600

    Generate models

---
+++ app/models/bookmark.rb
@@ -0,0 +1,6 @@
+class Bookmark < ApplicationRecord
+  belongs_to :movie
+  belongs_to :list
+  validates :comment, length: { minimum: 6 }
+  validates :movie, uniqueness: { scope: :list, message: "The combination movie/list must be unique"}
+end
